31012150ff3ad55435eb522e1c958cb1abd7cd3b
tahirburak2 <63349484+tahirburak2@users.noreply.github.com>
2024-10-30 01:53:21 +0300

    1. sayfa bitti

---
+++ Soru1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WEEK12HOMEWORK.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
